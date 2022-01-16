8e3c30cee3f592d086a3657488894c2ad286698b
baran <baran.dogan19007@gmail.com>
2022-01-16 14:50:03 +0300

    first commit

---
+++ FluentValidationApp.Web/FluentValidator/CustomerValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidationApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationApp.Web.FluentValidator
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name alanı boş geçilemez.");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Mail alanı boş geçilemez.").EmailAddress().WithMessage("Girilen mail, mail kurallarına uygun olmalıdır.");
+            RuleFor(x => x.Age).NotEmpty().WithMessage("Age alanı boş geçilemez.").InclusiveBetween(18, 65).WithMessage("Girilen yaş, 18-65 arası olmalıdır.");
+
+        }
+    }
+}
---
+++ FluentValidationApp.Web/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationApp.Web.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
