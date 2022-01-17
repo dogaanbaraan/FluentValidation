e8e94a99708e29b2c0e57759621122ba6d13ca91
baran <baran.dogan19007@gmail.com>
2022-01-17 23:26:40 +0300

    projection(birleştirme/bölme) işlemi yapıldı

---
+++ FluentValidationApp.Web/Mapping/EventDateProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using FluentValidationApp.Web.DTOs;
+using FluentValidationApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationApp.Web.Mapping
+{
+    public class EventDateProfile:Profile
+    {
+        public EventDateProfile()
+        {
+            CreateMap<EventDateDto, EventDate>()
+                   .ForMember(dest => dest.Date, opt => opt.MapFrom(x => new DateTime(x.Year, x.Month, x.Day)));
+
+            CreateMap<EventDate, EventDateDto>()
+                    .ForMember(dest => dest.Year, opt => opt.MapFrom(x => x.Date.Year))
+                    .ForMember(dest => dest.Month, opt => opt.MapFrom(x => x.Date.Month))
+                    .ForMember(dest => dest.Day, opt => opt.MapFrom(x => x.Date.Day));
+                
+                
+             
+        }
+    }
+}
